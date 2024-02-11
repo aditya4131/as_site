73521eb960abbea715971817f623ee1beb6968bf
Aditya Srivastava <aditya@Adityas-MacBook-Pro.local>
2024-02-10 20:28:51 -0600

    updated images

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,7 +7,7 @@ function HomePage() {
         <body id="homepage-body">
         <div className="app">
             <NavigationBar/>
-            <img src="https://hej4dtfnfqkde6jg.public.blob.vercel-storage.com/helloSticker.jpg" alt="person saying hello." id="person-waving-img"/>
+            <img src="https://hej4dtfnfqkde6jg.public.blob.vercel-storage.com/personwaving.png" alt="person saying hello." id="person-waving-img"/>
             <p id='homepage-info'>Software developer and <br/> undergraduate researcher at <br/> Louisiana State University.</p>
             <footer id="footer-note-1">
                 <p>Aditya Srivastava © 2024</p>

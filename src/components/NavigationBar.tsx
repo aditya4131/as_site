877a2c46a7ab8e0345d9f7af84949a8c34db2a2d
Aditya Srivastava <aditya@Adityas-MacBook-Pro.local>
2024-02-10 20:10:02 -0600

    updated site stlying for phones

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "./NavigationBar.css";
 
 function NavigationBar() {
@@ -9,19 +9,25 @@ function NavigationBar() {
       <ul id="home-button-nav">
         <li>
           {/* <a href="#">Aditya</a> */}
-          <Link to='/'>Aditya</Link>
+          <Link to="/">Aditya</Link>
         </li>
       </ul>
 
       <ul id="nav">
         <li>
-          <a href="https://drive.google.com/file/d/1u6tpN6lf_gTorqx5fotLF8-bS8sclwnh/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
+          <a
+            href="https://drive.google.com/file/d/1u6tpN6lf_gTorqx5fotLF8-bS8sclwnh/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Resume
+          </a>
         </li>
         <li>
-        <Link to='/experiences'>Experiences</Link>
+          <Link to="/experiences">Experiences</Link>
         </li>
         <li>
-         <Link to='/about'>About</Link>
+          <Link to="/about">About</Link>
         </li>
       </ul>
     </div>
